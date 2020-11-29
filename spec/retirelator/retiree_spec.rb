32557d48360bc783cb8d56e7c8e855489df96dc9
Carl Zulauf <carl@linkleaf.com>
2020-11-28 17:55:40 -0700

    Basic retiree coverage

--- spec/retirelator/retiree_spec.rb
+++ spec/retirelator/retiree_spec.rb
@@ -4,8 +4,15 @@ describe Retirelator::Retiree do
   end
 
   describe ".new" do
+    subject { described_class }
     it "can be initialized with no arguments" do
-      expect(described_class.new).to be_a(described_class)
+      expect(subject.new).to be_a(described_class)
+    end
+
+    it "can be initialized with a name and salary" do
+      obj = subject.new(name: "Chris", salary: 500_000)
+      expect(obj.name).to eq("Chris")
+      expect(obj.salary).to eq(500_000)
     end
   end
 end
