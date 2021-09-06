97d0a3163977c1bd1db3d9208b6d991b6912283a
Carl Zulauf <carl@linkleaf.com>
2021-09-06 15:56:04 -0600

    Handle decimals with commas

--- lib/retirelator/decimal.rb
+++ lib/retirelator/decimal.rb
@@ -1,7 +1,12 @@
 module Retirelator
   class Decimal
-    def self.from_hash(str)
-      str.to_d
+    def self.from_hash(value)
+      case value
+      when Numeric then value.to_d
+      when String then value.tr(',', '').to_d
+      else
+        value.to_d
+      end
     end
 
     def self.to_hash(decimal)
@@ -9,7 +14,7 @@ module Retirelator
     end
 
     def self.to_currency(decimal)
-      str = decimal.round(2).to_s("F")
+      str = to_string(decimal)
       left_of_decimal, right_of_decimal = str.split(".")
       leading_digits = left_of_decimal.length % 3
       remaining_digits = left_of_decimal[leading_digits, left_of_decimal.length]
@@ -20,7 +25,7 @@ module Retirelator
     end
 
     def self.to_string(decimal)
-      round(decimal).to_s
+      decimal.round(2).to_s("F")
     end
   end
 end
